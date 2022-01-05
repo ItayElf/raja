fc0ea815e31b802f98cba077b1e970ab6b577892
ItayElf <thh123321@gmail.com>
2022-01-05 20:07:44 +0200

    added closest commend suggestio

--- raja/actions/commands.py
+++ raja/actions/commands.py
@@ -1,10 +1,11 @@
+import difflib
 from typing import List
 
 from raja.actions import init
 
 _commands = {
     "init": init.init,
-    "destroy": init.destroy
+    "destroy": init.destroy,
 }
 
 
@@ -12,10 +13,13 @@ def handle_command(cmd: str, parans: List[str]) -> None:
     """Calls a command based on its name and the given parameters"""
     if cmd not in _commands:
         print(f"Illegal command: {cmd}.")
+        closest_matches_str = "\n\t".join(difflib.get_close_matches(cmd, _commands.keys()))
+        if closest_matches_str:
+            print(f"Similar commands: \n\t{closest_matches_str}")
         return
     try:
         _commands[cmd](*parans)
     except TypeError:
-        print(f"Illegal number of arguments to command {cmd}")
+        print(f"Illegal number of arguments to command {cmd}.")
     # except Exception as e:
     #     print(e)
