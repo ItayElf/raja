9ab562c3ea1152470fdc3e94ad3b1ecc8aba3f25
ItayElf <thh123321@gmail.com>
2022-01-23 14:30:08 +0200

    better commit msg

--- raja/actions/commit.py
+++ raja/actions/commit.py
@@ -1,9 +1,10 @@
 import json
 import os
 import sqlite3
+import time
 
 from raja.committer import create_commit, insert_commit
-from raja.utils import error
+from raja.utils import error, success
 
 
 def commit(message: str) -> None:
@@ -16,6 +17,7 @@ def commit(message: str) -> None:
     if not settings["username"]:
         error("Use 'raja config username <username>' before committing.")
         return
+    t1 = time.time()
     files = open(os.path.join(".raja", ".raja_files")).read().split("\n")
     with sqlite3.connect(os.path.join(".raja", ".raja_db")) as conn:
         c = create_commit(files, conn, settings["username"], message, settings["last_commit"])
@@ -23,3 +25,5 @@ def commit(message: str) -> None:
     with open(os.path.join(".raja", ".raja_settings.json"), "w") as f:
         settings["last_commit"] = c.hash
         json.dump(settings, f, indent=2)
+    t2 = time.time()
+    success(f"{len(c.file_changes)} changes saved ( {round(t2 - t1, 2)} s)")
