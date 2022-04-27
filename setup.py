29878a57b070f672b728757470dfb1aaefc4efe7
ItayElf <thh123321@gmail.com>
2022-04-27 10:17:36 +0300

    changes setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="raja",
     version="0.1.0",
-    packages=["raja"],
+    packages=["raja", "raja.actions", "raja.changes_finder", "raja.committer", "raja.file_handler", "raja.utils"],
     entry_points={
         'console_script': [
             "raja = raja.__main__:main"
